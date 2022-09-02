0db19313e98f25d1a13915792974f231c62360c0
Iraj <rustamahang@gmail.com>
2022-09-02 21:56:06 +0100

    Finished all activities but activity 5 with random number i need to solve

---
+++ activities.js
@@ -0,0 +1,87 @@
+//// ****** Activity 2 - Pet *****
+// const pet ={
+//     name: "Zeus",
+//     typeOfPet: "Dog",
+//     age: 3,
+//     colour: "White and Brouwn",
+//     eating: "Fish",
+//     drink: "Milk",
+//     eat(){
+//         return `${this.name} is eating a ${this.eating}`
+//     },
+//     drinking(){
+//         return `${this.name} is drinking a ${this.drink}`
+//     }  
+// };
+// console.log(pet.eat(), "and", pet.drinking())
+////*************************************************************** */
+
+// // ************** Activity 5 - Random number 1 and 30 **************  - ?????????????????? 
+
+// let arr = [];
+// while(arr.length < 6){
+//     let r = Math.floor(Math.random() * 30) + 1;
+//     if(arr.indexOf(r) === -1) arr.push(r);{
+//     }
+// }
+// if(arr % 7 == 0){
+//     console.log(`${arr} divisible by 7`)
+// }else{
+//     console.log(`${arr} not divisible by 7`)
+// }
+
+// // ********************************************************************
+
+// // ******************** Activity 1 SayHi ****************************
+
+// const person = {
+//     name: "Rus",
+//     age: 29,
+//     sayHi(){
+//         return `Hello my name is ${this.name}`
+//     }
+// }
+// console.log(person.sayHi())
+
+// // ****************************************************************************
+
+// // ******************** Activity 2 - 6 Random numbers ********************
+// let arr = [];
+// while(arr.length < 6){
+//     let r = Math.floor(Math.random() * 50) + 1;
+//     if(arr.indexOf(r) === -1) arr.push(r);{
+//     }
+// }
+// console.log(arr)
+// //********************************************************************** */
+
+// // ************************** Activity 3 ***********************
+// for (let i = 10; i > 0; i -= 1) {
+//     if (i === 0) {
+//       break;
+//     }
+//     console.log(i);
+//     }
+// //************************************************************ */
+
+// // ******************** Activity 2 - What I eat?? *************
+// let meal = [
+//     "Fish",
+//     "Chicken",
+//     "Fruits"
+// ]
+// console.log(`Today I eat ${meal[0]} for a breakfast, ${meal[1]} for a lunch and ${meal[2]} for a dinner.`)
+// //*********************************************************************************************************** */
+
+// // ************************** Activity 1 - Favourite Films *********************
+// let favMovie = [
+//     "Thor",
+//     "Blade",
+//     "War Craft"
+// ]
+// favMovie.splice(4, 0, "Facebook", "Zadiac")
+// for(let i = 0; i < favMovie.length; i++){
+//     console.log(favMovie[i])
+// }
+// // ********************************************************************
+
